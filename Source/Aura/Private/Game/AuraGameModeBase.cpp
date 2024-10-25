cdd1c42003105072b4c7d7ea4c86c825a8b39920
guilhermepferreira <guilhermep.ferreiraa@gmail.com>
2024-10-25 03:54:28 +0100

    Aula 13

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright SorBakaM Games
+
+
+#include "Game/AuraGameModeBase.h"
+
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -5,12 +5,13 @@
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
 #include "AuraEnemy.generated.h"
+#include "Interaction/EnemyInterface.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
